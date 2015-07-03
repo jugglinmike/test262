3270addbec637bd1cf5b695dd90d54e0fe70c25a
Mike Pennisi <mike@mikepennisi.com>
2015-07-03 15:26:35 -0400

    Improve test

--- test/language/literals/regexp/u-case-mapping.js
+++ test/language/literals/regexp/u-case-mapping.js
@@ -18,5 +18,32 @@ info: >
            b. Else, return ch.
 ---*/
 
-assert(/\u212a/iu.test('k'));
-assert(/\u212a/iu.test('K'));
+assert.sameValue(
+  /\u212a/i.test('k'),
+  false,
+  'Case mapping is not applied in the absense of the `u` flag'
+);
+assert.sameValue(
+  /\u212a/i.test('K'),
+  false,
+  'Case mapping is not applied in the absense of the `u` flag'
+);
+assert.sameValue(
+  /\u212a/u.test('k'),
+  false,
+  'Case mapping is not applied in the absense of the `i` flag'
+);
+assert.sameValue(
+  /\u212a/u.test('K'),
+  false,
+  'Case mapping is not applied in the absense of the `i` flag'
+);
+
+assert(
+  /\u212a/iu.test('k'),
+  'Case mapping is applied in the presence of the `i` and `u` flags'
+);
+assert(
+  /\u212a/iu.test('K'),
+  'Case mapping is applied in the presence of the `i` and `u` flags'
+);

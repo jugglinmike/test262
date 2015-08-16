de833fd2b37b87ddc64cf1034db7e5c64a1f8ae2
Mike Pennisi <mike@mikepennisi.com>
2015-08-16 18:29:07 -0400

    fixup! Add tests for well-known Symbol: @@toPrimitive

--- test/language/expressions/equals/coerce-symbol-to-prim-invocation.js
+++ test/language/expressions/equals/coerce-symbol-to-prim-invocation.js
@@ -27,6 +27,7 @@ features: [Symbol.toPrimitive]
 ---*/
 
 var y = {};
+var callCount = 0;
 var thisVal, args;
 
 y[Symbol.toPrimitive] = function() {
@@ -35,6 +36,12 @@ y[Symbol.toPrimitive] = function() {
 };
 
 0 == y;
-assert.sameValue(thisVal, y);
-assert.sameValue(args.length, 1);
-assert.sameValue(args[0], 'default');
+
+assert.sameValue(callCount, 1, 'method invoked exactly once');
+assert.sameValue(thisVal, y, '`this` value is the object being compared');
+assert.sameValue(args.length, 1, 'method invoked with exactly one argument');
+assert.sameValue(
+  args[0],
+  'default',
+  'method invoked with the string "default" as the first argument'
+);

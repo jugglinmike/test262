ec01424a6511807647c84820d0c1608c376d03fa
Mike Pennisi <mike@mikepennisi.com>
2016-01-20 14:36:49 -0500

    Improve formatting

--- test/language/statements/for-in/cptn-decl-zero-itr.js
+++ test/language/statements/for-in/cptn-decl-zero-itr.js
@@ -23,6 +23,5 @@ info: >
        c. If nextResult is false, return NormalCompletion(V).
 ---*/
 
-
 assert.sameValue(eval('1; for (var a in {}) { }'), undefined);
 assert.sameValue(eval('1; for (var b in {}) { 2; }'), undefined);

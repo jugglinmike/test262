c857151218d4015c0ff896bce6cf3e5209fb7842
Mike Pennisi <mike@mikepennisi.com>
2016-01-21 14:08:08 -0500

    Add another test

--- test/language/statements/with/cptn-empty-abrupt.js
+++ test/language/statements/with/cptn-empty-abrupt.js
@@ -2,7 +2,8 @@
 // This code is governed by the BSD license found in the LICENSE file.
 /*---
 es6id: 13.11.7
-description: Statement completion value
+description: >
+    Statement completion value when body returns an empty abrupt completion
 info: >
     WithStatement : with ( Expression ) Statement
 
@@ -11,9 +12,19 @@ info: >
     9. Set the running execution context’s Lexical Environment to oldEnv.
     10. If C.[[type]] is normal and C.[[value]] is empty, return
         NormalCompletion(undefined).
-    11. Return Completion(C).
 flags: [noStrict]
 ---*/
 
-assert.sameValue(eval('1; with({}) { }'), undefined);
-assert.sameValue(eval('2; with({}) { 3; }'), 3);
+assert.sameValue(
+  eval('1; do { 2; with({}) { 3; break; } 4; } while (false);'), 3
+);
+assert.sameValue(
+  eval('5; do { 6; with({}) { break; } 7; } while (false);'), 6
+);
+
+assert.sameValue(
+  eval('8; do { 9; with({}) { 10; continue; } 11; } while (false);'), 10
+);
+assert.sameValue(
+  eval('12; do { 13; with({}) { continue; } 14; } while (false);'), 13
+);

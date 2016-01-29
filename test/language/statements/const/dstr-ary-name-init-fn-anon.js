f5e5bfef173806be6cc5ee80f2fc28d8a9c4da6d
Travis CI <contact@travis-ci.com>
2016-01-29 00:26:34 +0000

    Re-build from source

---
+++ test/language/statements/const/dstr-ary-name-init-fn-anon.js
@@ -0,0 +1,38 @@
+// This file was procedurally generated from the following sources:
+// - src/cases/dstr-binding/ary-name-init-fn-anon.hashes
+// - src/cases/dstr-binding/default/const-stmt.hashes
+/*---
+description: SingleNameBinding assigns inferred name to anonymous functions (`const` statement)
+es6id: 13.3.1.4
+info: >
+    13.3.3.6 Runtime Semantics: IteratorBindingInitialization
+    
+    SingleNameBinding : BindingIdentifier Initializeropt
+    
+    [...]
+    6. If Initializer is present and v is undefined, then
+       a. Let defaultValue be the result of evaluating Initializer.
+       b. Let v be GetValue(defaultValue).
+       c. ReturnIfAbrupt(v).
+       d. If IsAnonymousFunctionDefinition(Initializer) is true, then
+          i. Let hasNameProperty be HasOwnProperty(v, "name").
+          ii. ReturnIfAbrupt(hasNameProperty).
+          iii. If hasNameProperty is false, perform SetFunctionName(v,
+               bindingId).
+    7. If environment is undefined, return PutValue(lhs, v).
+    8. Return InitializeReferencedBinding(lhs, v).
+
+    LexicalBinding : BindingPattern Initializer
+    
+    1. Let rhs be the result of evaluating Initializer.
+    2. Let value be GetValue(rhs).
+    3. ReturnIfAbrupt(value).
+    4. Let env be the running execution context's LexicalEnvironment.
+    5. Return the result of performing BindingInitialization for BindingPattern
+       using value and env as the arguments.
+---*/
+
+
+const [x = function() {}] = [];
+
+assert.sameValue(x.name, 'x');
---
+++ test/language/statements/variable/dstr-ary-name-init-fn-anon.js
@@ -0,0 +1,37 @@
+// This file was procedurally generated from the following sources:
+// - src/cases/dstr-binding/ary-name-init-fn-anon.hashes
+// - src/cases/dstr-binding/default/var-stmt.hashes
+/*---
+description: SingleNameBinding assigns inferred name to anonymous functions (`var` statement)
+es6id: 13.3.2.4
+info: >
+    13.3.3.6 Runtime Semantics: IteratorBindingInitialization
+    
+    SingleNameBinding : BindingIdentifier Initializeropt
+    
+    [...]
+    6. If Initializer is present and v is undefined, then
+       a. Let defaultValue be the result of evaluating Initializer.
+       b. Let v be GetValue(defaultValue).
+       c. ReturnIfAbrupt(v).
+       d. If IsAnonymousFunctionDefinition(Initializer) is true, then
+          i. Let hasNameProperty be HasOwnProperty(v, "name").
+          ii. ReturnIfAbrupt(hasNameProperty).
+          iii. If hasNameProperty is false, perform SetFunctionName(v,
+               bindingId).
+    7. If environment is undefined, return PutValue(lhs, v).
+    8. Return InitializeReferencedBinding(lhs, v).
+
+    VariableDeclaration : BindingPattern Initializer
+    
+    1. Let rhs be the result of evaluating Initializer.
+    2. Let rval be GetValue(rhs).
+    3. ReturnIfAbrupt(rval).
+    4. Return the result of performing BindingInitialization for
+       BindingPattern passing rval and undefined as arguments.
+---*/
+
+
+var [x = function() {}] = [];
+
+assert.sameValue(x.name, 'x');

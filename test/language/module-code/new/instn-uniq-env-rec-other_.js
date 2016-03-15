205757e2b2a3fe01cf8af2ed20410df0504a60ae
Mike Pennisi <mike@mikepennisi.com>
2016-03-15 10:19:34 -0400

    Add license boilerplate

--- test/language/module-code/new/instn-uniq-env-rec-other_.js
+++ test/language/module-code/new/instn-uniq-env-rec-other_.js
@@ -1,3 +1,6 @@
+// Copyright (C) 2016 the V8 project authors. All rights reserved.
+// This code is governed by the BSD license found in the LICENSE file.
+
 var first = 4;
 let second = 5;
 const third = 6;

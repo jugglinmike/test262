e26ae4be90dfd74854e8ad7cef7e21353d7e0c27
Mike Pennisi <mike@mikepennisi.com>
2016-03-16 12:29:29 -0400

    Simplify `make.py`

--- make.py
+++ make.py
@@ -4,8 +4,6 @@ import os, shutil, subprocess, sys
 
 OUT_DIR = os.environ.get('OUT_DIR') or 'test'
 SRC_DIR = os.environ.get('SRC_DIR') or 'src'
-PUBLISH_DIR = os.environ.get('PLUBLISH_DIR') or 'test'
-UPSTREAM = os.environ.get('UPSTREAM') or 'git@github.com:tc39/test262.git'
 
 def shell(*args):
     sp = subprocess.Popen(list(args), stdout=subprocess.PIPE)
@@ -35,30 +33,16 @@ def target(*deps):
         return wrapped
     return other
 
-@target('build_static', 'build_cases')
-def build():
-    pass
-
 @target()
-def build_cases():
+def build():
     shell(sys.executable, 'tools/generation/generator.py',
+          'create',
           '--out', OUT_DIR,
           SRC_DIR)
 
 @target()
 def clean():
     shell(sys.executable, 'tools/generation/generator.py', 'clean', OUT_DIR)
-
-@target('clean', 'build')
-def deploy():
-    shutil.move(OUT_DIR, OUT_DIR + '.tmp')
-    shell('git', 'checkout', 'master')
-    shutil.rmtree(PUBLISH_DIR)
-    shutil.move(OUT_DIR + '.tmp', OUT_DIR)
-    shell('git', 'add', '--all', PUBLISH_DIR)
-    shell('git', 'commit', '--message', '"Re-build from source"')
-    shell('git', 'push', UPSTREAM, 'master')
-    shell('git', 'checkout', '-')
 
 if len(sys.argv) == 1:
     targets['build']()

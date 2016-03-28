d39c3e271fd3415cea97313d8acb1d84eac19669
Mike Pennisi <mike@mikepennisi.com>
2016-03-28 15:16:31 -0400

    Correct erroneous assertion

--- test/language/module-code/new/eval-export-dflt-expr-gen-named.js
+++ test/language/module-code/new/eval-export-dflt-expr-gen-named.js
@@ -28,5 +28,5 @@ flags: [module]
 export default (function* gName() { return 88; });
 import g from './eval-export-dflt-expr-gen-named.js';
 
-assert.sameValue(g(), 88, 'binding initialized');
+assert.sameValue(g().next().value, 88, 'binding initialized');
 assert.sameValue(g.name, 'gName', 'correct name is assigned');

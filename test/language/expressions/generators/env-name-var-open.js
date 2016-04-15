34530fde39f8aad1404f09b161e7d8912e75afa4
Mike Pennisi <mike@mikepennisi.com>
2016-04-15 17:07:24 -0400

    Add tests for generator function expression's name

---
+++ test/language/expressions/generators/env-name-var-open.js
@@ -0,0 +1,31 @@
+// Copyright (C) 2016 the V8 project authors. All rights reserved.
+// This code is governed by the BSD license found in the LICENSE file.
+/*---
+esid: sec-generator-function-definitions-runtime-semantics-evaluation
+description: >
+    Creation of new variable environment for the BindingIdentifier
+    parameter
+info: |
+    [...]
+    2. Let scope be the running execution context's LexicalEnvironment.
+    3. Let funcEnv be NewDeclarativeEnvironment(scope).
+    4. Let envRec be funcEnv's EnvironmentRecord.
+    5. Let name be StringValue of BindingIdentifier.
+    6. Perform envRec.CreateImmutableBinding(name, false).
+    7. Let closure be GeneratorFunctionCreate(Normal, FormalParameters,
+       GeneratorBody, funcEnv, strict).
+    [...]
+---*/
+
+var f = 'outside';
+var probe1 = function() { return f; };
+var probe2;
+
+var func = function* f(_ = probe2 = function() { return f; }) {
+  return 'inside';
+};
+
+func().next();
+
+assert.sameValue(probe1(), 'outside');
+assert.sameValue(probe2(), func);
